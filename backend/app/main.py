6c808c5cfe24f7ba4575e23b1b3893e2f35db5a3
Mohamed Aslam <146819895+mohamedaslam2k4@users.noreply.github.com>
2026-08-16 12:54:16 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # cors (cross origin resorce sharing)=>to allow origins and  prevent error
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:5173","http://127.0.0.1:5173",],
+    allow_origins=[ "https://truck-load-hub.onrender.com","http://127.0.0.1:5173",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
