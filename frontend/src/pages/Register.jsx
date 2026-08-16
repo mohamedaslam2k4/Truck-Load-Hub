8a3a3552d3d094a1ab02afa89ca400d265572d0a
Mohamed Aslam <146819895+mohamedaslam2k4@users.noreply.github.com>
2026-08-16 17:51:09 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "/logo.png";
+import { API_URL } from "../api";
 
 const initialFormData = {
   name: "",
