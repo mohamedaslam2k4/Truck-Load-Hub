fac560ac200dfb9bab8cde074bdb6e5310148052
Mohamed Aslam <146819895+mohamedaslam2k4@users.noreply.github.com>
2026-08-18 10:58:54 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { API_URL } from "../api"; // Centralized API import
+import { API_URL } from "../api"; 
+import logo from "/logo.png";
 
 function Login({ setUserRole }) {
   const navigate = useNavigate();
